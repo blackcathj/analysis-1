f5269bdfde12cdc55a528974b30d0ca7845957e9
Jin Huang <jhuang@bnl.gov>
2021-11-23 11:44:36 -0500

    read macro

---
+++ eIDML/macro/Fun4All_ReadDST_eIDML.C
@@ -0,0 +1,65 @@
+#ifndef MACRO_FUN4ALL_READDST_C
+#define MACRO_FUN4ALL_READDST_C
+
+
+#include <GlobalVariables.C>
+#include <G4_Input.C>
+
+#include <eidml/eIDMLInterface.h>
+
+#include <TROOT.h>
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllServer.h>
+
+#include <phool/recoConsts.h>
+
+
+R__LOAD_LIBRARY(libfun4all.so)
+// Use libg4eicdst for campaign 2 DSTs
+R__LOAD_LIBRARY(libg4eicdst.so)
+// Use libg4dst for campaign 1 DSTs
+//R__LOAD_LIBRARY(libg4dst.so)
+R__LOAD_LIBRARY(libeidml.so)
+
+
+
+int Fun4All_ReadDST_eIDML(const int nEvents = 10,
+		    const string& inputFile = "singlePion.lst",
+		    const string &outputFile = "G4EICDetector.root")
+
+{
+  //---------------
+  // Fun4All server
+  //---------------
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(1);
+
+  // just if we set some flags somewhere in this macro
+  recoConsts *rc = recoConsts::instance();
+  
+  Input::READHITS = true;
+  INPUTREADHITS::listfile[0] = inputFile;
+
+  eIDMLInterface *anaTutorial = new eIDMLInterface("anaTutorial", outputFile + "_anaTutorial.root");
+  anaTutorial->setMinJetPt(3.);
+  anaTutorial->Verbosity(0);
+  anaTutorial->analyzeTracks(true);
+  anaTutorial->analyzeClusters(true);
+  anaTutorial->analyzeJets(true);
+  anaTutorial->analyzeTruth(false);
+  se->registerSubsystem(anaTutorial);
+
+  InputManagers();
+
+  se->run(nEvents);
+
+  se->End();
+  
+  delete se;
+  std::cout << "All done processing" << std::endl;
+  gSystem->Exit(0);
+  return 0;
+
+}
+#endif
