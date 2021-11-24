f952dcea4a9769015e9e6cb919062877819d1504
Jin Huang <jhuang@bnl.gov>
2021-11-24 11:38:21 -0500

    add a plot

--- eIDML/macro/Fun4All_ReadDST_eIDML.C
+++ eIDML/macro/Fun4All_ReadDST_eIDML.C
@@ -24,7 +24,7 @@ R__LOAD_LIBRARY(libeidml.so)
 
 
 
-int Fun4All_ReadDST_eIDML(const int nEvents = 2000,
+int Fun4All_ReadDST_eIDML(const int nEvents = 200000,
 //    const string& inputFile = "singleElectron.lst"//
         const string& inputFile = "singlePion.lst"//
 		        )
